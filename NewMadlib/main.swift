4e92ff7e2e2ad7651f4d6dd330a365b89ca4d9e7
cstaker <cstaker@coupeville.k12.wa.us>
2015-12-15 09:51:26 -0800

    Initial Commit

---
+++ NewMadlib/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NewMadlib
+//
+//  Created by Cody Staker on 12/15/15.
+//  Copyright © 2015 Cody Staker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
